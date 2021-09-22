758f877e1320ca314c386dd23af422d53e393cc3
NISHITH A <nishithza@gmail.com>
2021-09-22 09:57:55 +0530

    Create pattern1.java

---
+++ pattern1.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+class GFG {
+	public static void main (String[] args) {
+	    int count =1;
+		
+		for (int i=0;i<2;i++){
+		    for(int j=0;j<3;j++){
+		        System.out.print(count+" ");
+		        count++;
+		    }
+		    System.out.println("");
+		}
+	}
+}
